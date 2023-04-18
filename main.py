0de60dd0f12ddbc130158b148cee2c5c3c4664f9
FireFall <firekonstantin@mail.ru>
2023-04-18 16:49:10 +0300

    updated mainMenu and Button class

--- main.py
+++ main.py
@@ -90,22 +90,29 @@ for x in range(1, 100):
 
 #defining fonts functions
 #prerender text to return as Surface
-def cacheFont(text: str, color = (255, 255, 255), size=24, antialiasing=True):
+def cacheFont(text, color = (255, 255, 255), size=24, antialiasing=True) -> pygame.Surface:
 	return fonts[size].render(text, antialiasing, color)
 
-#render prerendered text(see cacheFont) to surface(default screen)
+#render prerendered text(see cacheFont) to surface(default: screen)
 def renderFont(render, pos, surface: pygame.Surface = screen):
 	surface.blit(render, pos)
 
 #defining classes
 #UI classes
 class Button:
-	def __init__(self, pos, width, text):
+	def __init__(self, pos, text, width=None, autoresizeOffset=8, callback=None):
 		#saving arguments as variables
 		self.pos = pos
-		self.width = width
+		if width != None: self.width = width
 		self.text = text
 		self.active = False
+		self.callback = callback
+
+		#prerendering text
+		self.text = cacheFont(text)
+
+		#defining width if None
+		if self.width == None: self.width = self.text.get_width()+autoresizeOffset
 
 		#defining button rects
 		self.rect = pygame.Rect(pos, (self.width, 64))
@@ -114,9 +121,11 @@ class Button:
 		self.cornerRightRect = pygame.Rect((pos[0]+(self.width-16), pos[1]), (16, 64))
 
 		#scaling body texture
-		self.bodyTexture = pygame.transform.scale(buttonBody, (width-32, 64))
-		self.bodyTextureActive = pygame.transform.scale(buttonBodyActive, (width-32, 64))
-	def render(self, screen):
+		self.bodyTexture = pygame.transform.scale(buttonBody, (self.width-32, 64))
+		self.bodyTextureActive = pygame.transform.scale(buttonBodyActive, (self.width-32, 64))
+
+		self.textRect = pygame.Rect((pos[0]+self.rect.w//2-self.text.get_width()//2, pos[1]+(self.rect.h//2-self.text.get_height()//2)), (self.text.get_width(), self.text.get_height()))
+	def render(self, screen: pygame.Surface = screen):
 		#checking if button is hovered by mouse
 		if self.rect.collidepoint(pygame.mouse.get_pos()):
 			self.active = True
@@ -132,20 +141,47 @@ class Button:
 			screen.blit(buttonCornerLeft, self.cornerLeftRect)
 			screen.blit(self.bodyTexture, self.bodyRect)
 			screen.blit(buttonCornerRight, self.cornerRightRect)
+		#rendering text
+		renderFont(self.text, self.textRect)
+	def eventHold(self, event):
+		if event.type == MOUSEBUTTONDOWN and self.rect.collidepoint(pygame.mouse.get_pos()):
+			self.callback()
 
 #stopping loading screen after setting things up
 loadingScreen = False
 
 def mainMenu():
-	testButton = Button((16, 16), 256, "bober")
+	logo.set_alpha(0)
+	
+	#defining buttons
+	playBtn = Button((12, logo.get_height()+24), "Play", 240)
+	settingsBtn = Button((12, playBtn.rect.bottom+4), "Settings", 240)
+	aboutBtn = Button((12, settingsBtn.rect.bottom+4), "About", 240)
+	exitBtn = Button((12, aboutBtn.rect.bottom+4), "Exit", 240, callback=exit)
 	while 1:
 		clock.tick(60)
 		screen.fill((28, 21, 53))
+
+		screen.blit(logo, (12, 12))
+
+		#logo appear animation
+		if logo.get_alpha() < 255:
+			logo.set_alpha(logo.get_alpha()+5)
+
 		for event in pygame.event.get():
+			# playBtn.eventHold(event)
+			# settingsBtn.eventHold(event)
+			# aboutBtn.eventHold(event)
+			exitBtn.eventHold(event)
 			if event.type == QUIT:
 				pygame.quit()
 				exit()
-		testButton.render(screen)
+		
+		playBtn.render()
+		settingsBtn.render()
+		aboutBtn.render()
+		exitBtn.render()
+		
 		screen.blit(cursor, pygame.mouse.get_pos())
 		pygame.display.update()
 	
