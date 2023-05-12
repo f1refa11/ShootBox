3cc29d607854fc21efd442bfc49565a29e8e8440
FireFall <firekonstantin@mail.ru>
2023-05-12 12:39:00 +0300

    Fixes and project structure optimizations
    - Fixed resourcePath variable
    - Button widget and its textures were moved to widgets/button.py for better project structure

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def gameExit():
 
 #configuring essential paths
 rootPath = os.path.dirname(__file__)
-resourcesPath = os.path.join(rootPath, "resources")
+resourcesPath = os.path.join(rootPath, "assets")
 texturesPath = os.path.join(resourcesPath, "textures")
 uiTexturesPath = os.path.join(texturesPath, "ui")
 
@@ -101,12 +101,6 @@ playerPath = os.path.join(texturesPath, "player")
 #loading all assets
 #ui assets
 cursor = loadPathTexture(uiTexturesPath, "cursor.png", True, (64, 64))
-buttonCornerLeft = loadPathTexture(uiTexturesPath, "buttonCorner.png", True, (16, 64))
-buttonCornerLeftActive = loadPathTexture(uiTexturesPath, "buttonCornerActive.png", True, (16, 64))
-buttonCornerRight = pygame.transform.flip(buttonCornerLeft, True, False)
-buttonCornerRightActive = pygame.transform.flip(buttonCornerLeftActive, True, False)
-buttonBody = loadPathTexture(uiTexturesPath, "buttonBody.png", True, (16, 64))
-buttonBodyActive = loadPathTexture(uiTexturesPath, "buttonBodyActive.png", True, (16, 64))
 
 #ground assets
 grass = loadPathTexture(blocksPath, "grass.png", True, (64, 64))
@@ -138,56 +132,9 @@ CHUNKSIZE = 8
 #defining variables
 fps = 75
 
-#defining classes
-#UI classes
-class Button:
-	def __init__(self, pos, text, width=None, autoresizeOffset=8, callback=None):
-		#saving arguments as variables
-		self.pos = pos
-		if width != None: self.width = width
-		self.text = text
-		self.active = False
-		self.callback = callback
-
-		#prerendering text
-		self.text = cacheFont(text)
-
-		#defining width if None
-		if self.width == None: self.width = self.text.get_width()+autoresizeOffset
-
-		#defining button rects
-		self.rect = pygame.Rect(pos, (self.width, 64))
-		self.cornerLeftRect = pygame.Rect(pos, (16, 64))
-		self.bodyRect = pygame.Rect((pos[0]+16, pos[1]), (self.width-32, 64))
-		self.cornerRightRect = pygame.Rect((pos[0]+(self.width-16), pos[1]), (16, 64))
-
-		#scaling body texture
-		self.bodyTexture = pygame.transform.scale(buttonBody, (self.width-32, 64))
-		self.bodyTextureActive = pygame.transform.scale(buttonBodyActive, (self.width-32, 64))
-
-		self.textRect = pygame.Rect((pos[0]+self.rect.w//2-self.text.get_width()//2, pos[1]+(self.rect.h//2-self.text.get_height()//2)), (self.text.get_width(), self.text.get_height()))
-	def render(self, screen: pygame.Surface = screen):
-		#checking if button is hovered by mouse
-		if self.rect.collidepoint(pygame.mouse.get_pos()):
-			self.active = True
-		else:
-			self.active = False
-		
-		#rendering button depending on state
-		if self.active:
-			screen.blit(buttonCornerLeftActive, self.cornerLeftRect)
-			screen.blit(self.bodyTextureActive, self.bodyRect)
-			screen.blit(buttonCornerRightActive, self.cornerRightRect)
-		else:
-			screen.blit(buttonCornerLeft, self.cornerLeftRect)
-			screen.blit(self.bodyTexture, self.bodyRect)
-			screen.blit(buttonCornerRight, self.cornerRightRect)
-		#rendering text
-		renderFont(self.text, self.textRect)
-	def eventHold(self, event):
-		#running callback if mouse clicked on button
-		if event.type == MOUSEBUTTONDOWN and self.rect.collidepoint(pygame.mouse.get_pos()):
-			self.callback()
+#importing modules
+#widgets
+from widgets.button import Button
 
 #player class
 class Player:
