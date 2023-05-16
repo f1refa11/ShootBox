c04e09b00d4f6d232289d267833bc44b6d0d87fe
FireFall <firekonstantin@mail.ru>
2023-05-16 18:05:32 +0300

    Optimized fontmgr.py

--- fontmgr.py
+++ fontmgr.py
@@ -1,6 +1,5 @@
 import pygame,os
 from paths import resourcesPath
-from main import screen
 fonts = []
 for x in range(1, 100):
 	fonts.append(pygame.font.Font(os.path.join(resourcesPath, "font.ttf"), x))
@@ -10,5 +9,5 @@ def cacheFont(text, color = (255, 255, 255), size=24, antialiasing=True) -> pyga
 	return fonts[size].render(text, antialiasing, color)
 
 #render prerendered text(see cacheFont) to surface(default: screen)
-def renderFont(render, pos, surface: pygame.Surface = screen):
+def renderFont(render, pos, surface: pygame.Surface):
 	surface.blit(render, pos)
