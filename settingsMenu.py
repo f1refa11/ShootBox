cff662fd2149544816727ee0a32be5b0e0c06106
FireFall <firekonstantin@mail.ru>
2023-05-18 12:49:00 +0300

    Create settings menu

---
+++ settingsMenu.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame import QUIT
+from funcs import gameExit
+from fontmgr import cacheFont,renderFont
+from button import Button
+def settingsMenu():
+	from main import cursor,logo,screen,clock
+	from mainMenu import mainMenu
+	from graphicsSettings import graphicsSettings
+	from soundSettings import soundSettings
+	from langSettings import langSettings
+	backButton = Button((20,20), "Back", callback=mainMenu)
+	graphicsButton = Button((20,140), "Graphics", 240, callback=graphicsSettings)
+	soundButton = Button((20,210), "Sound", 240, callback=soundSettings)
+	langButton = Button((20,280), "Language", 240, callback=langSettings)
+	title = cacheFont("Settings",size=32)
+	while 1:
+		clock.tick(75)
+		screen.fill((28, 21, 53))
+
+		renderFont(title, (20,92), screen)
+		backButton.render(screen)
+		graphicsButton.render(screen)
+		soundButton.render(screen)
+		langButton.render(screen)
+
+		for event in pygame.event.get():
+			backButton.eventHold(event)
+			if event.type == QUIT:
+				gameExit()
+		
+		screen.blit(cursor, pygame.mouse.get_pos())
+		pygame.display.update()
