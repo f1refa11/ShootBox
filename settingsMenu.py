5e7fa007bc725721764a1784b8575343d8aab2c3
FireFall <firekonstantin@mail.ru>
2023-08-24 21:52:57 +0300

    Make settingsMenu functional
    by calling eventHold for all the buttons in the menu

--- settingsMenu.py
+++ settingsMenu.py
@@ -3,17 +3,19 @@ from pygame import QUIT
 from funcs import gameExit
 from fontmgr import cacheFont,renderFont
 from widgets.button import Button
-from confvar import fpsLimit
+from confmgr import fpsLimit
 def settingsMenu():
 	from main import cursor,logo,screen,clock
 	from mainMenu import mainMenu
 	from graphicsSettings import graphicsSettings
 	from soundSettings import soundSettings
 	from langSettings import langSettings
+	from modulesMenu import modulesMenu
 	backButton = Button((20,20), "Back", callback=mainMenu)
 	graphicsButton = Button((20,140), "Graphics", 240, callback=graphicsSettings)
 	soundButton = Button((20,210), "Sound", 240, callback=soundSettings)
 	langButton = Button((20,280), "Language", 240, callback=langSettings)
+	modulesButton = Button((20,350), "Modules", 240, callback=modulesMenu)
 	title = cacheFont("Settings",size=32)
 	while 1:
 		clock.tick(fpsLimit)
@@ -24,9 +26,14 @@ def settingsMenu():
 		graphicsButton.render(screen)
 		soundButton.render(screen)
 		langButton.render(screen)
+		modulesButton.render(screen)
 
 		for event in pygame.event.get():
 			backButton.eventHold(event)
+			graphicsButton.eventHold(event)
+			soundButton.eventHold(event)
+			langButton.eventHold(event)
+			modulesButton.eventHold(event)
 			if event.type == QUIT:
 				gameExit()
 		
