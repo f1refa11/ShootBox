9ad63232276a92bbf62d068c849b99e36b3f30dd
FireFall <firekonstantin@mail.ru>
2023-11-25 12:01:34 +0300

    Update player.py
    - put player to the window center
    - make player rotate towards the mouse
    - add direction arrow to show player direction

--- player.py
+++ player.py
@@ -1,29 +1,59 @@
 import pygame
 from funcs import loadPathTexture
-from paths import *
+from paths import playerPath
+import math
+from confmgr import showDirectionArrow
+import screenmgr
 playerIdle = loadPathTexture(playerPath, "idle.png", True, (64, 64))
+if showDirectionArrow != 0: playerDirection = loadPathTexture(playerPath, "direction.png", True, (64,64)).convert_alpha()
 class Player:
 	def __init__(self, pos) -> None:
 		self.x, self.y = pos
 		self.speed = 3
 		self.rect = pygame.Rect(self.x, self.y, 64, 64)
+
+		self.rendX, self.rendY = screenmgr.width//2-32, screenmgr.height//2-32
+
+		self.arrowCount = 0
+		self.arrowOpacity = 255
+
+		self.inventory = []
+		self.health = 100
 	def collisionCheck():
-		# if self.rect
 		pass
 	def up(self):
-		# if not self.rect.top < 3:
 		self.y -= self.speed
 	def down(self):
 		pass
 	def left(self):
-		# if not self.rect.left < 3:
 		self.x -= self.speed
 	def right(self):
 		pass
 	def render(self, surface: pygame.Surface):
+		mx,my = pygame.mouse.get_pos()
+		dx,dy = mx-self.rendX-32, my-self.rendY-32
+		angle = -math.degrees(math.atan2(dy,dx))-90
+		tempTexture = pygame.transform.rotozoom(playerIdle, angle, 1).convert_alpha()
+		if showDirectionArrow != 0: tempTextureDirection = pygame.transform.rotozoom(playerDirection, angle, 1).convert_alpha()
+		if showDirectionArrow == 1:
+			tempTextureDirection.set_alpha(self.arrowOpacity)
+			if self.arrowCount < 9:
+				if self.arrowCount % 2 == 0:
+					if self.arrowOpacity > 0:
+						self.arrowOpacity -= 10
+					else:
+						self.arrowOpacity = 0
+						self.arrowCount += 1
+				else:
+					if self.arrowOpacity < 255:
+						self.arrowOpacity += 10
+					else:
+						self.arrowOpacity = 255
+						self.arrowCount += 1
+		self.renderRect = tempTexture.get_rect(centerx=self.rendX+32,centery=self.rendY+32)
 		#updating changed(possibly) variables
 		self.rect = pygame.Rect(self.x, self.y, 64, 64)
 
 		#rendering to screen
-		surface.blit(playerIdle, (self.x, self.y))
-		pygame.draw.rect(surface, (255, 0, 0), self.rect, 1)
+		surface.blit(tempTexture, self.renderRect)
+		if showDirectionArrow != 0: surface.blit(tempTextureDirection, self.renderRect)
